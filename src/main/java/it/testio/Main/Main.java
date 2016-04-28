b878db516f224561a22d91d850779cbdbbbeceea
Bobalex <lionbridgeomsk@gmail.com>
2016-04-28 16:20:36 +0600

    second commit

---
+++ src/main/java/it/testio/Main/Main.java
@@ -0,0 +1,10 @@
+package it.testio.Main;
+
+/**
+ * Main class
+ */
+public class Main {
+    public static void main(final String[] args){
+            System.out.print("Hello,world!");
+        }
+}
